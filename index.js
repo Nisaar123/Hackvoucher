1fa295a113f9111ccaa520173d7dd878b71bc53e
Nisar Ahmad <n-------n@Nisars-MacBook-Pro.local>
2022-05-23 05:24:04 +0530

    port2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express') ;
 const cookieParser = require('cookie-parser') ;
-const port = Process.env.PORT || 4000 ;
+const port = process.env.PORT || 4000 ;
 const app = express() ;
 
 const routes = require('./routes/home') ;
